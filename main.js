bf0bc5ee5c3b0e4488cbd7ad18ea0a2103fb1a5a
prasad6302 <poojariprasad630@gmail.com>
2025-12-11 12:27:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,175 @@
+// console.log('Hello from the external js');
+// console.log(name)
+// var name = 'Prasad'
+// console.log(age)
+// var age = 23
+
+
+// function wish(){
+//     return "Good morning..."
+// }
+// console.log(wish());
+
+// let isLogin = null
+// console.log(isLogin);
+
+
+
+
+// let arr = [1,4,2,6,23,"hello",true]
+// arr.push(7)
+// console.log(arr);
+
+// arr.unshift(0)
+// console.log(arr);
+
+
+// let x = 5
+// let y = x
+
+// y = 10
+
+// console.log(x);
+// console.log(y);
+// console.log(x === y);
+
+// let obj1 = {name:'Prasad'}
+// let obj2 = obj1
+// obj2.name = 'wish'
+// console.log(obj1);
+// console.log(obj2);
+
+
+// console.log(obj1 === obj2);
+
+
+// console.log(5 === 5);
+// console.log(true === true);
+// console.log("hi" === "hi");
+
+
+// let arr = [1,2]
+// let arr2 = [1,2]
+// let arr3 = arr
+// console.log('This is same reference',arr === arr3);//true
+
+// console.log(arr === arr2);//This condition will always return 'false' since JavaScript compares objects by reference, not value.
+// obj1 = {}
+// obj2 = {}
+// console.log(obj1 === obj2);//This condition will always return 'false' since JavaScript compares objects by reference, not value.
+
+// console.log([1] === [1]);
+
+
+
+// let arr = [1,2,3,4,5]
+// let result = arr.slice(1,3);
+// // output: [3, 4, 5]
+// console.log(result);
+
+
+// console.log(typeof 55);
+// console.log(typeof "Hello");
+// console.log(typeof true);
+// console.log(typeof 10n);
+// console.log(typeof null);
+
+// var : redeclaration reinitialization
+// let 
+// const
+
+// var arr;
+// // console.log(arr);
+// arr = 1
+// console.log(arr);
+
+// var arr = 10
+// // console.log(arr);
+// arr = 20
+// console.log(arr);
+
+// let
+// ecma script 2016
+// let arr = 10
+// arr = 20
+// // console.log(arr);
+// let arr = 40
+
+// const 
+// const arr = 10
+// arr = 20
+// console.log(arr);
+
+
+// let arr = 10
+
+// function wish(){
+//     arr = 20
+//     let abc = 40
+//     console.log(arr);
+    
+// }
+// console.log(abc);
+
+// wish()
+
+
+// let a = 123
+// console.log(a);
+// console.log(typeof a);
+
+// let b = String(a)
+// console.log(b);
+// console.log(typeof b);
+
+
+// let a = 0
+// console.log(a);
+// console.log(typeof a);
+// let b = Boolean(a)
+// console.log(b);
+// console.log( typeof b);
+
+
+// let success = false
+// if (success){
+//     console.log("Login success");
+    
+// }
+// else{
+//     console.log("Login failed");
+    
+// }
+
+
+// let num1 = 10
+// let num2 = 20
+// let num3 = "30"
+
+// console.log(num1 + num2 - num3);
+
+
+// let num = 1
+// let con = true
+
+// console.log(num == con);
+
+
+// let num = 0
+// let con = false
+// console.log(num == con);
+
+
+// let num1 = 10
+// let num2 = 
+ 
+
+
+
+
+
+
+
+
+
+
